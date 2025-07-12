d6e25592c55dccd2e37d0de1af643c915c3ebba5
waghmaresiddhi <waghmaresiddhi07@gmail.com>
2025-07-12 19:39:57 +0530

    Update settings.py

--- backend/trade_backend/settings.py
+++ backend/trade_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2(g+5g&rse#1-t44+s2d09c&4gjdwe0sy)40#ac=23iujuek6v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
